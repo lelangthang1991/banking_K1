646b5d5d8d0b161209bc6efe284b16e43270331f
bstardev\anh.nguyen <anh.nguyen@gbstarsolutions.com>
2022-05-26 17:09:11 +0700

    [feature/account_delete] wrire api account delete

--- src/main/java/com/bstar/banking/controller/AccountController.java
+++ src/main/java/com/bstar/banking/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.bstar.banking.model.response.ResponsePageAccount;
 import com.bstar.banking.model.response.RestResponse;
 import com.bstar.banking.service.AccountService;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,48 +25,14 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/check-pin")
-    public RestResponse<PinCodeResponse> checkPinCode(@Valid @RequestBody PinCodeDTO pinCodeDTO) {
-        return accountService.checkPinCode(pinCodeDTO);
-    }
-
-    /**
-     * @param keyword
-     * @param pageNumber
-     * @param pageSize
-     * @return page Account with keyword
-     * http://localhost:8080/api/v1/accounts/find-by-keyword?keyword=123456789&page=1&size=5
-     */
-    @GetMapping("/find-by-keyword")
-    public RestResponse<ResponsePageAccount> findAccountByKeyword(@RequestParam(required = false) String keyword,
-                                                                  @RequestParam int pageNumber,
-                                                                  @RequestParam int pageSize) {
-        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
-        return accountService.findAccountByKeyword(keyword, pageRequest);
-    }
-
-    @GetMapping("/activated/find-by-keyword")
-    public RestResponse<ResponsePageAccount> findAccountByKeywordActivated(@RequestParam(required = false) String keyword,
-                                                                           @RequestParam boolean isActivated,
-                                                                           @RequestParam int pageNumber,
-                                                                           @RequestParam int pageSize) {
-        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
-        return accountService.findAccountByKeywordAndActivated(keyword, isActivated, pageRequest);
+    public RestResponse<PinCodeResponse> checkPinCode(@Valid @RequestBody PinCodeDTO pinCodeDTO, Authentication authentication) {
+        return accountService.checkPinCode(pinCodeDTO, authentication);
     }
 
     @GetMapping("/get-by-email")
     public RestResponse<CommonResponse> getAccountByEmail(Authentication authentication){
         return accountService.findAccountByEmail(authentication.getName());
-    }
-
-    @GetMapping("/get-page")
-    public RestResponse<ResponsePageAccount> getPageAccount(@RequestParam int pageNumber,
-                                                        @RequestParam int pageSize){
-        return accountService.findPageAccount(PageRequest.of(pageNumber, pageSize));
-    }
-
-    @GetMapping("/{accountNumber}")
-    public RestResponse<CommonResponse> getAccountDetail(@PathVariable("accountNumber") String accountNumber){
-        return accountService.findAccountByAccountNumber(accountNumber);
     }
 }
